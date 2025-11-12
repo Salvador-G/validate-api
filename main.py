5fc82558a696fffe0dfcb4b89a85cd95da1b7f54
salvador gh <sal.guerrero.h26@gmail.com>
2025-11-12 14:56:38 -0500

    crear el app antes de las tablas

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from app.system.db import Base, engine
 from app.modules.auth.routes import router as auth_router
 from fastapi import FastAPI
 
-
-# Crear las tablas si no existen (solo para pruebas iniciales)
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
+# Crear tablas al iniciar la aplicación
+@app.on_event("startup")
+def startup_event():
+    print("🔹 Creando tablas si no existen...")
+    Base.metadata.create_all(bind=engine)
 
-
-# Registrar los routers
-app.include_router(auth_router) 
+# Registrar routers
+app.include_router(auth_router)
